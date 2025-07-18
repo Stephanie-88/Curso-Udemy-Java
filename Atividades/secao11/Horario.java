9e57cdf224cfec3edf2354d764f2b52a6e8676f2
Stéphanie Christine <72025387+Stephanie-88@users.noreply.github.com>
2025-07-18 16:49:36 -0300

    Instanciando data e hora

---
+++ Atividades/secao11/Horario.java
@@ -0,0 +1,34 @@
+import java.time.*;
+
+public class Horario {
+    public static void main(String[] args) {
+        //DateTimeFormatter olhar documentação
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+      
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+        
+        LocalDate d04 = LocalDate.parse("2025-07-02");
+        LocalDateTime d05 = LocalDateTime.parse("2025-07-02T01:30:26");
+        Instant d06 = Instant.parse("2025-07-02T01:30:26Z");
+        Instant d07 = Instant.parse("2025-07-02T01:30:26-03:00");
+        
+        LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
+        LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+        
+        LocalDate d10 = LocalDate.of(2025, 8, 20);
+        
+        System.out.println("d01 = " + d01);
+        System.out.println("d02 = " + d02);
+        System.out.println("d03 = " + d03);
+        System.out.println("d04 = " + d04);
+        System.out.println("d05 = " + d05);
+        System.out.println("d06 = " + d06);
+        System.out.println("d07 = " + d07);
+        System.out.println("d08 = " + d08);
+        System.out.println("d09 = " + d09);
+        System.out.println("d10 = " + d10);
+    }
+}
